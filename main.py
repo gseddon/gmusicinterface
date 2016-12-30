1d7b28213b5f5283790b988bff7fb03da4eb6b39
Gareth Seddon <gareth.seddon@gmail.com>
2016-12-30 18:27:48 +1100

    works with streaming and saving

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 from gmusicapi import Mobileclient
 import configparser
-
 import vlc
 
-
-class Test():
-    p = None
-    def __init__(self):
-        api = Mobileclient()
-        print("initialised")
-        api.login('gareth.seddon@gmail.com', 'pmzk993ne', Mobileclient.FROM_MAC_ADDRESS)
-        print("logged in")
-        library = api.get_all_songs()
-        sweet_track_ids = [track for track in library if track['artist'] == 'Flume']
-
-        id = sweet_track_ids[0]["id"]
-
-        url = api.get_stream_url(id)
-        print(sweet_track_ids[0]["artist"] + "\n" + url)
-        self.instance = vlc.Instance(url)
-        # self.p = vlc.MediaPlayer(url)
-        # self.p.play()
-        self.instance.media_player_new().play
 
 
 
 if __name__ == "__main__":
-    # test = Test()
     config = configparser.ConfigParser()
     config.read("account.ini")
     account = config["Account"]
-    print(account["username"] + ", " + account["password"])
+    api = Mobileclient()
+    print("initialised", account["username"], account["password"])
+    api.login(account["username"], account["password"], Mobileclient.FROM_MAC_ADDRESS)
+    print("logged in")
+    library = api.get_all_songs()
+    sweet_track_ids = [track for track in library if track['artist'] == 'Flume']
+
+    id = sweet_track_ids[0]["id"]
+
+    url = api.get_stream_url(id)
+    print(sweet_track_ids[0]["artist"] + "\n" + url)
+    instance = vlc.Instance("--sout=#duplicate{dst=file{dst=example.mpg},dst=display}")
+    media = instance.media_new(url)
+    player = instance.media_player_new()
+    player.set_media(media)
+    player.play()
+
+    # self.p = vlc.MediaPlayer(url)
+    # self.p.play()
+
